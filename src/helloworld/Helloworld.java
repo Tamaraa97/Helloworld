3383279a953cbd171e2da879aae49f3847ac467d
Tamaraa97 <pecniktamara1997@yahoo.com>
2020-02-19 21:18:23 +0100

    First commit

---
+++ src/helloworld/Helloworld.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!!!");
+		System.out.print("Prvi program");
+		System.out.println(" Testing");
+		System.out.println(" With selenium");
+		System.out.println(344);
+	}
+
+}
